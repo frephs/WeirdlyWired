8898ee07744a4e1177b43552a99438c910d1af7c
Francesco <ffrancescogenovese@gmail.com>
2022-02-18 14:53:59 +0100

    Fixed  intersection, added descriptions

--- core.py
+++ core.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup, builder
 from datetime import date
 
 def getUrl(word):
+    "Returns that is to be scraped"
     return "https://sapere.virgilio.it/parole/sinonimi-e-contrari/"+word.strip()
 
-def getSomeSynonymsOf(word, level, sinonimo):
+def getSomeSynonymsOf(word, level, synonym):
+    "Scrapes synonyms or contraries of a given word"
     found = []
     word = word.capitalize()
     # Let's get scraping
@@ -14,7 +16,7 @@ def getSomeSynonymsOf(word, level, sinonimo):
     soup = BeautifulSoup(content, 'html.parser')
     box = soup.findAll('div', attrs = {'class':'sct-descr'})
     try:
-        if sinonimo:
+        if synonym:
             box = soup.findAll('p')[1]
             # print(box.text)
             sinonimi = box.find_all("a" )
@@ -33,6 +35,7 @@ def getSomeSynonymsOf(word, level, sinonimo):
     return found
 
 def alreadyPresent(words, word):
+    "Checks if (word, level, reps) is already present in the set of words"
     found = False
     for w in words:
         #print("Checking: "+ w[0].capitalize() + word[0].capitalize() )
@@ -41,60 +44,68 @@ def alreadyPresent(words, word):
             w[2] += 1
     return found
 
-def _getWords(words, word, level, limit, sinonimo):
-    words.append([word, level, 1])
-    if level < limit:
-        syns = getSomeSynonymsOf(word, level+1, sinonimo)
+def _getWords(words, word, level, maxRecursion, synonym):
+    """ 
+        Recursively looks for more words 
+        (synonms if synonym==true otherwise contraries ), 
+        Stops when the maxRecursion is reached.
+    """
+    if not alreadyPresent(words, [word, level, 1]):
+        words.append([word.capitalize(), level, 1])        
+    if level < maxRecursion:
+        syns = getSomeSynonymsOf(word, level+1, synonym)
         for s in syns:
             if not alreadyPresent(words, s):
-                if not sinonimo:
-                    sinonimo = True
-                _getWords(words, s[0], level+1, limit, sinonimo) 
+                if not synonym:
+                    synonym = True
+                _getWords(words, s[0], level+1, maxRecursion, synonym) 
     return words
 
-def getWords(words, word, level, limit, sinonimo):
-    words = _getWords(words, word, level, limit, sinonimo)
+def getWords(words, word, level, maxRecursion, synonym):
+    "Get synonyms or contraries sorts them and returns them"
+    words = _getWords(words, word, level, maxRecursion, synonym)
     sorter = lambda x: (x[1], -x[2])
     res = sorted(words, key=sorter)
     return res;
 
 def intersect(words_a, words_list):
+    """
+    Intersects a set of words with a list of others sets, 
+    if a word is present in all the sets, its added
+    """
     intersection = []
     for a in words_a:
-        found = True
-        # print(words_list)
         for words_b in words_list:
-            #print(words_b)
             found = alreadyPresent(words_b, a);
             if not found:
-                #print("Did not find" + a[0]+ " in")
-                # print(words_b)
-                break;
+                break; # I HATE PYTHON SO MUCH
         if found:
-            intersection.append(a[0])
-            # print(a[0])
+            intersection.append(a[0].capitalize())
     return intersection
 
-def findIntersection(str, precision):
-    words = str.split(", ");
-    #print(words)
-    n_words = len(words)
-    words_list = []
-    intersection = []
+def findIntersection(str, precision, maxRecursion):
+    """
+        Given a list of words separated by commas finds a list of synonyms recursively
+        Intersects the sets and continues until the minimum number of words (precision) is met
+        or maxRecursion is reached
+    """
+    words = str.split(", "); n_words = len(words);
+    words_list = []; intersection = [];
     for w in words:
-        words_list.append([]);
-    while (not intersection) and ( len(intersection) < precision) :
+        words_list.append([[w, 0, 1]]);
+    iterations = 1
+    while ((not intersection) or ( len(intersection) < precision)) and (iterations<=maxRecursion):
         for i in range(0, n_words):
-            words_list[i] = getWords(words_list[i], words[i], 0, 1, True)
+            for w in words_list[i]:
+                words_list[i] = _getWords(words_list[i], w[0], w[1]+1, iterations, True)
         a = words_list[0]
-        b = [words_list[i] for i in range(0,n_words) if i]
-        # print(words_list)
-        #print("\n\n"); print(a)
+        b = [words_list[i] for i in range(0,n_words) if i] # all sets of words except the first one
         intersection = intersect(a,b)
+        iterations+=1
     return intersection
 
 def main():
-    print(findIntersection("Pigro, fannullone, scansafatiche", 2))
+    print(findIntersection("Metro, quadro", 5, 6))
 
 if __name__ == "__main__":
     main()
